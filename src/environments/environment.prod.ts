39dffba3be778d3d63d4875ab68b1b3beba8ea45
CrisGonzaVal <cgonzalezvalenzuela4@gmail.com>
2024-11-21 01:10:44 -0300

    se redirige el apiUrl y se implementa capacitor/android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  //enlazar con android
+  //apiUrl:'http://localhost:3000'
+  apiUrl:'https://repodata010v-1-puvd.onrender.com'
+
 };
