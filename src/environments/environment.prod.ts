9251f43fa2ebaa354f5d036886c6adb4332725db
CrisGonzaVal <cgonzalezvalenzuela4@gmail.com>
2024-11-26 04:56:18 -0300

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //enlazar con android
  // apiUrl:'http://localhost:3000'
- apiUrl:'https://repodata-4x18.onrender.com'
+ apiUrl:'https://repodata-rqrl.onrender.com'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl:'http://localhost:3000'
-  apiUrl:'https://repodata-4x18.onrender.com'
+  apiUrl:'https://repodata-rqrl.onrender.com'
 };
 
 /*
